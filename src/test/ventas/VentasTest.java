711c64a78ae6b911bfb7b2c126845a64d63a81e3
Maxi Insfran <maxiinsfran@gmail.com>
2022-09-19 11:35:30 -0300

    firts commit

---
+++ src/test/ventas/VentasTest.java
@@ -0,0 +1,16 @@
+package test.ventas;
+
+import ventas.Orden;
+import ventas.Producto;
+
+public class VentasTest {
+    public static void main(String[] args) {
+        Producto producto1 = new Producto("Camisa", 50.00);
+        Producto producto2 = new Producto("Pantalon", 100.00);
+
+        Orden orden1 = new Orden();
+        orden1.agregarProducto(producto1);
+        orden1.agregarProducto(producto2);
+        orden1.mostrarOrden();
+    }
+}
